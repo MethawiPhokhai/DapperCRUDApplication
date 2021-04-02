a9b38e093fb2023fa557fca8f627ee7c0ce9cb29
Methawi <topfee1234@gmail.com>
2021-04-02 14:03:18 +0700

    Add Code first Connection DB

---
+++ Context/EmployeeDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DapperCRUDApplication.Context
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        DbSet<Employee> Employees { get; set; }
+    }
+}
+
--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,9 @@
+using DapperCRUDApplication.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -32,6 +34,8 @@ namespace DapperCRUDApplication
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "DapperCRUDApplication", Version = "v1" });
             });
+            services.AddMvc();
+            services.AddDbContext<EmployeeDbContext>(item => item.UseSqlServer(Configuration.GetConnectionString("DapperConn")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
